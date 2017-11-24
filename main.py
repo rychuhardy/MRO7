dc5d30d94112c425435929a89118e9d3b1e4b694
ry <ryszard.sikora@hotmail.com>
2017-11-24 21:18:45 +0100

    done

--- main.py
+++ main.py
@@ -4,15 +4,34 @@ matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 from skimage import io
 from pyclustering.cluster.kmedians import kmedians
+from sklearn.decomposition import PCA
 
 # https://pypi.python.org/pypi/pyclustering
 # https://github.com/annoviko/pyclustering/blob/master/pyclustering/cluster/examples/kmedians_examples.py
-img = io.imread('AlaskaLynx_EN-US9313111559_1920x1080.jpg', as_grey=False)
-dd = img.reshape(1920*1080, 3)
+img = np.array(io.imread('photo.jpg', as_grey=False), dtype="int64")
+img_reshaped = img.reshape(180*180, 3)
 
 # optionally remove duplicates
+k = 20
+np.random.shuffle(img_reshaped)
+start_points = img_reshaped[:k]
 
-kmedians_instance = kmedians(dd, [[255, 0, 0], [255, 255,0], [0,0,255], [0,255,255]])
+kmedians_instance = kmedians(img_reshaped, start_points)
 kmedians_instance.process()
 clusters = kmedians_instance.get_clusters()
+medians = kmedians_instance.get_medians()
 print(img.shape)
+
+
+pca = PCA(n_components=2)
+reduced = pca.fit_transform(img_reshaped)
+
+kmedians_result = sum([list(zip(idx, np.tile(c, (len(idx), 1)))) for idx, c in list(zip(clusters, medians))], [])
+kmedians_X = np.array([reduced[idx] for idx, c in kmedians_result])
+kmedians_colors = [c/255 for idx, c in kmedians_result]
+
+plt.clf()
+fig, ax = plt.subplots(figsize=(16,16))
+ax.scatter(kmedians_X[:, 0], kmedians_X[:, 1], marker='s', s=50, color=kmedians_colors)
+
+plt.show()
